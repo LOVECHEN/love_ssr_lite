576b9365ebc4e39711d8139017c84edd33655b4f
LOVE <LOVE@LOVEdeMacBook-Pro.local>
2016-08-07 12:00:30 +0800

    fix

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-test
+#install
+
+安装方法
+
+git clone https://github.com/LOVEChen/love_ssr_lite.git
+
+docker pull lovechen/love_ssr_lite
+
+vi conf/config.sh
+
+change "your_password" with any strings you want
+
+modify port at startd.sh
+
+and fill the right conf path
+
+sh startd.sh
+
+
+
+if you want do something with this,do command:
+sh goto.sh
+
+if you want kill the process,do command:
+sh killall.sh
+
+
+HAVE FUN!
--- startd.sh
+++ startd.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 fs_mount="/root/docker-ssr/conf"
-ssh_port="22222"
 ssr_port="9090"
 ssr_config=config
-cntr=$1
+cntr=lovechen/love_ssr_lite
 
 docker kill $(docker ps -q)
-docker run -d -v ${fs_mount}:/conf -p ${ssh_port}:22 -p ${ssr_port}:8989 -p ${ssr_port}:8989/udp ${cntr} init-start ${ssr_config}.json
+docker run -d -v ${fs_mount}:/conf -p ${ssr_port}:8989 -p ${ssr_port}:8989/udp ${cntr} init-start ${ssr_config}.json
